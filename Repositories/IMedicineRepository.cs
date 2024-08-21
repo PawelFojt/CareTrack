ca653218d1b32202258c872bbd3e42391c3e6aab
Pawel Fojt <pwfojt@gmail.com>
2024-08-21 22:55:11 +0200

    s

--- Repositories/IMedicineRepository.cs
+++ Repositories/IMedicineRepository.cs
@@ -4,7 +4,7 @@ namespace CareTrack.Server.Repositories;
 
 public interface IMedicineRepository
 {
-    Task<Result<List<IMedicine>>> GetList();
+    Task<Result<List<MedicineResult>>> GetList();
     Task<Result<IMedicine>> Add(IMedicine medicine);
     Task<Result<IMedicine>> Update(IMedicine medicine);
     Task<Result<IMedicine>> Delete(int id);
