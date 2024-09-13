f8a9a89dcee22d55b8d19a34c759bb4b7671d64e
Pawel Fojt <pwfojt@gmail.com>
2024-09-13 20:26:21 +0200

    change AddPatientCommand request

--- Modules/Application/Patient/Command/AddPatientCommand.cs
+++ Modules/Application/Patient/Command/AddPatientCommand.cs
@@ -6,7 +6,14 @@ namespace CareTrack.Server.Modules.Application.Patient.Command;
 
 public class AddPatientCommand : IRequest<Result<IPatient>>
 {
-    public PatientResult PatientResult { get; set; }
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public int Age { get; set; }
+    public int Weight { get; set; }
+    public string PhoneNumber { get; set; } = string.Empty;
+    public DateTime Admission { get; set; }
+    public DateTime Discharge { get; set; }
 }
 
 public class AddPatientCommandHandler
@@ -19,7 +26,18 @@ public class AddPatientCommandHandler
     }
     public async Task<Result<IPatient>> Handle(AddPatientCommand request, CancellationToken cancellationToken)
     {
-        var patient = await patientRepository.Add(request.PatientResult);
-        return patient;
+        var patient = new PatientResult
+        {
+            Id = request.Id,
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Age = request.Age,
+            Weight = request.Weight,
+            PhoneNumber = request.PhoneNumber,
+            Admission = request.Admission,
+            Discharge = request.Discharge
+        };
+        var patientResult = await patientRepository.Add(patient);
+        return patientResult;
     }
 }
